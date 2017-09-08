57dea2ce501c1fea86a463d22954f79bd292dd95
levin <levin@qq.com>
2017-09-08 16:31:28 +0800

    Add Method Content

--- DevelopUtils/app/src/main/java/com/levin/developutils/Utils/AppUtils.java
+++ DevelopUtils/app/src/main/java/com/levin/developutils/Utils/AppUtils.java
@@ -1,8 +1,485 @@
 package com.levin.developutils.Utils;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.graphics.Rect;
+import android.os.Build;
+import android.telephony.TelephonyManager;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.lang.reflect.Method;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static android.content.Context.ACTIVITY_SERVICE;
+
 /**
  * Created by levin on 2017/6/15.
+ * app 开发中经常遇到的一些方法
  */
 
 public class AppUtils {
+
+    /**
+     * 获取当前时间并转换为毫秒值
+     *
+     * @return
+     */
+    public static String getCurrentTime() {
+        try {
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddhhmm");
+            String date = sdf.format(new Date());
+            long millionSeconds = sdf.parse(date).getTime();//毫秒
+            return String.valueOf(millionSeconds);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 将毫秒值转换成需要的时间格式
+     *
+     * @return
+     */
+    public static String getCurrentTimeByMillisconds(long millionSeconds, String pattern) {
+        try {
+            SimpleDateFormat sdf = new SimpleDateFormat(pattern);
+            String date = sdf.format(new Date(millionSeconds));
+            return date;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 获取当前时间  "yyyy/MM/dd HH:mm"
+     * 区间模式时间筛选
+     */
+    public static String getCurrentTimeString() {
+        Date date = new Date();
+        SimpleDateFormat formatDate = new SimpleDateFormat("yyyy/MM/dd HH:mm");
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        return formatDate.format(cal.getTime());
+    }
+
+    /**
+     * dp转成px
+     *
+     * @param dipValue
+     * @return
+     */
+    public static int dip2px(Context context, float dipValue) {
+        return (int) (dipValue * context.getResources().getDisplayMetrics().density + 0.5f);
+    }
+
+    /**
+     * px转成dp
+     *
+     * @param pxValue
+     * @return
+     */
+    public static int px2dip(Context context, float pxValue) {
+        return (int) (pxValue / context.getResources().getDisplayMetrics().density + 0.5f);
+    }
+
+    /**
+     * 将px值转换为sp值，保证文字大小不变
+     *
+     * @param pxValue
+     * @return
+     */
+    public static int px2sp(Context context, float pxValue) {
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (pxValue / fontScale + 0.5f);
+    }
+
+    /**
+     * 将sp值转换为px值，保证文字大小不变
+     *
+     * @param spValue
+     * @return
+     */
+    public static int sp2px(Context context, float spValue) {
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (spValue * fontScale + 0.5f);
+    }
+
+    /**
+     * 将字符串转成MD5值
+     *
+     * @param string
+     * @return
+     */
+    public static String stringToMD5(String string) {
+        try {
+            if (!TextUtils.isEmpty(string)) {
+                byte[] hash = MessageDigest.getInstance("MD5").digest(string.getBytes("UTF-8"));
+                StringBuilder hex = new StringBuilder(hash.length * 2);
+                for (byte b : hash) {
+                    if ((b & 0xFF) < 0x10)
+                        hex.append("0");
+                    hex.append(Integer.toHexString(b & 0xFF));
+                }
+
+                return hex.toString();
+            }
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return null;
+    }
+
+    /**
+     * 动态设置view margin
+     *
+     * @param v
+     * @param l
+     * @param t
+     * @param r
+     * @param b
+     */
+    public static void setMargins(View v, int l, int t, int r, int b) {
+        if (v.getLayoutParams() instanceof ViewGroup.MarginLayoutParams) {
+            ViewGroup.MarginLayoutParams p = (ViewGroup.MarginLayoutParams) v.getLayoutParams();
+            p.setMargins(l, t, r, b);
+            v.requestLayout();
+        }
+    }
+
+    /**
+     * 获取设备号
+     *
+     * @return
+     */
+    public static String getDeviceId(Context context) {
+        TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(
+                Context.TELEPHONY_SERVICE);
+        return telephonyManager.getDeviceId();
+    }
+
+    /**
+     * 获取设备唯一uuid
+     *
+     * @return
+     */
+    public static String getAppDeviceId(Context context) {
+        DeviceUuidFactory uuidFactory = new DeviceUuidFactory(context);
+        UUID uuid = uuidFactory.getDeviceUuid();
+        return uuid.toString();
+    }
+
+    /**
+     * 获取前台页面
+     *
+     * @param context
+     * @return
+     */
+    public static String getForegroundActivity(Context context) {
+        try {
+            ActivityManager am = (ActivityManager) context.getSystemService(ACTIVITY_SERVICE);
+            List<ActivityManager.RunningTaskInfo> cn = am.getRunningTasks(1);
+            ActivityManager.RunningTaskInfo taskInfo = cn.get(0);
+            ComponentName name = taskInfo.topActivity;
+            Activity foregroundActivity = (Activity) (Class.forName(name.getClassName()).newInstance());
+            return foregroundActivity.getClass().getName().toString();
+
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 检测手机号是否合法
+     *
+     * @param str
+     * @return
+     */
+    public static boolean isVaildMobile(String str) {
+        Pattern pattern = Pattern.compile("[1][34578]\\d{9}");
+        Matcher matcher = pattern.matcher(str);
+        if (matcher.matches()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * 动态设置下view margin
+     *
+     * @param view
+     * @param marginTop
+     * @param marginBottom
+     */
+    public static void setViewMarginLL(Context mContext, View view, int marginTop, int marginBottom, int marginLeft, int marginRight) {
+        LinearLayout.LayoutParams lp = (LinearLayout.LayoutParams) view.getLayoutParams();
+        lp.setMargins(
+                dip2px(mContext, marginLeft),
+                dip2px(mContext, marginTop),
+                dip2px(mContext, marginRight),
+                dip2px(mContext, marginBottom));
+        view.setLayoutParams(lp);
+    }
+
+    /**
+     * 动态设置下view margin
+     *
+     * @param view
+     * @param marginTop
+     * @param marginBottom
+     */
+    public static void setViewMarginFL(Context mContext, View view, int marginTop, int marginBottom, int marginLeft, int marginRight) {
+        FrameLayout.LayoutParams lp = (FrameLayout.LayoutParams) view.getLayoutParams();
+        lp.setMargins(
+                dip2px(mContext, marginLeft),
+                dip2px(mContext, marginTop),
+                dip2px(mContext, marginRight),
+                dip2px(mContext, marginBottom));
+        view.setLayoutParams(lp);
+    }
+
+    public static boolean checkPermission(Context context, String permission) {
+        boolean result = false;
+        if (Build.VERSION.SDK_INT >= 23) {
+            try {
+                Class<?> clazz = Class.forName("android.content.Context");
+                Method method = clazz.getMethod("checkSelfPermission", String.class);
+                int rest = (Integer) method.invoke(context, permission);
+                if (rest == PackageManager.PERMISSION_GRANTED) {
+                    result = true;
+                } else {
+                    result = false;
+                }
+            } catch (Exception e) {
+                result = false;
+            }
+        } else {
+            PackageManager pm = context.getPackageManager();
+            if (pm.checkPermission(permission, context.getPackageName()) == PackageManager.PERMISSION_GRANTED) {
+                result = true;
+            }
+        }
+        return result;
+    }
+
+    public static String getDeviceInfo(Context context) {
+        try {
+            org.json.JSONObject json = new org.json.JSONObject();
+            TelephonyManager tm = (TelephonyManager) context
+                    .getSystemService(Context.TELEPHONY_SERVICE);
+            String device_id = null;
+            if (checkPermission(context, Manifest.permission.READ_PHONE_STATE)) {
+                device_id = tm.getDeviceId();
+            }
+            String mac = null;
+            FileReader fstream = null;
+            try {
+                fstream = new FileReader("/sys/class/net/wlan0/address");
+            } catch (FileNotFoundException e) {
+                fstream = new FileReader("/sys/class/net/eth0/address");
+            }
+            BufferedReader in = null;
+            if (fstream != null) {
+                try {
+                    in = new BufferedReader(fstream, 1024);
+                    mac = in.readLine();
+                } catch (IOException e) {
+                } finally {
+                    if (fstream != null) {
+                        try {
+                            fstream.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    if (in != null) {
+                        try {
+                            in.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+            json.put("mac", mac);
+            if (TextUtils.isEmpty(device_id)) {
+                device_id = mac;
+            }
+            if (TextUtils.isEmpty(device_id)) {
+                device_id = android.provider.Settings.Secure.getString(context.getContentResolver(),
+                        android.provider.Settings.Secure.ANDROID_ID);
+            }
+            json.put("device_id", device_id);
+            return json.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 返回当前程序版本名
+     */
+    public static String getAppVersionName(Context context) {
+        String versionName = "";
+        try {
+            // ---get the package info---
+            PackageManager pm = context.getPackageManager();
+            PackageInfo pi = pm.getPackageInfo(context.getPackageName(), 0);
+            versionName = pi.versionName;
+//            int versioncode = pi.versionCode;
+            if (versionName == null || versionName.length() <= 0) {
+                return "";
+            }
+        } catch (Exception e) {
+        }
+        return versionName;
+    }
+
+
+    /**
+     * 重新回到app 当前页面
+     *
+     * @param context
+     * @param packageName
+     * @param className
+     */
+    public static void redirectToAppCurrentPage(Context context, String packageName, String className) {
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_LAUNCHER);
+        ComponentName cn = new ComponentName(packageName, className);
+        intent.setComponent(cn);
+        context.startActivity(intent);
+    }
+
+    /**
+     * 启动当前包名的app
+     *
+     * @param context
+     * @param packageName
+     */
+    private void doStartApplicationWithPackageName(Context context, String packageName) {
+        // 通过包名获取此APP详细信息，包括Activities、services、versioncode、name等等
+        PackageInfo packageinfo = null;
+        try {
+            packageinfo = context.getPackageManager().getPackageInfo(packageName, 0);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        if (packageinfo == null) {
+            return;
+        }
+
+        // 创建一个类别为CATEGORY_LAUNCHER的该包名的Intent
+        Intent resolveIntent = new Intent(Intent.ACTION_MAIN, null);
+        resolveIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+        resolveIntent.setPackage(packageinfo.packageName);
+
+        // 通过getPackageManager()的queryIntentActivities方法遍历
+        List<ResolveInfo> resolveinfoList = context.getPackageManager()
+                .queryIntentActivities(resolveIntent, 0);
+
+        ResolveInfo resolveinfo = resolveinfoList.iterator().next();
+        if (resolveinfo != null) {
+            // packageName = 参数sPackName
+            String sPackName = resolveinfo.activityInfo.packageName;
+            // 这个就是我们要找的该APP的LAUNCHER的Activity[组织形式：packagename.mainActivityname]
+            String className = resolveinfo.activityInfo.name;
+            // LAUNCHER Intent
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+            // 设置ComponentName参数1:packagename参数2:MainActivity路径
+            ComponentName cn = new ComponentName(sPackName, className);
+
+            intent.setComponent(cn);
+            context.startActivity(intent);
+        }
+    }
+
+    /**
+     * 让页面整个弹出在键盘上方不遮盖底部按钮
+     * 1、获取main在窗体的可视区域
+     * 2、获取main在窗体的不可视区域高度
+     * 3、判断不可视区域高度
+     * 1、大于100：键盘显示  获取Scroll的窗体坐标
+     * 算出main需要滚动的高度，使scroll显示。
+     * 2、小于100：键盘隐藏
+     *
+     * @param main   根布局
+     * @param scroll 需要显示的最下方View
+     */
+    public static void addLayoutListener(final View main, final View scroll) {
+        main.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                Rect rect = new Rect();
+                main.getWindowVisibleDisplayFrame(rect);
+                int mainInvisibleHeight = main.getRootView().getHeight() - rect.bottom;
+                if (mainInvisibleHeight > 100) {
+                    int[] location = new int[2];
+                    scroll.getLocationInWindow(location);
+                    int srollHeight = (location[1] + scroll.getHeight()) - rect.bottom;
+                    main.scrollTo(0, srollHeight);
+                } else {
+                    main.scrollTo(0, 0);
+                }
+            }
+        });
+    }
+
+    /**
+     * 字符串时间 转换long
+     *
+     * @param pattern 时间格式
+     * @param s       时间字符串
+     * @return
+     */
+    public static long StringToLongTime(String pattern, String s) {
+        Date time = null;
+        SimpleDateFormat sd = new SimpleDateFormat(pattern);
+        try {
+            time = sd.parse(s);
+        } catch (java.text.ParseException e) {
+            e.printStackTrace();
+        }
+        return time.getTime();
+    }
 }
